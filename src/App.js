3c58664e81322433408e76f1b6b7fe43380e8876
saadlarbi <saadux@gmail.com>
2022-04-30 23:30:50 +0200

    agg07

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {ThemeProvider, Container, Navbar} from 'react-bootstrap';
+import logo from './logo.png';
+
 function App() {
   return (
-    <div>
-      <h1>AGG 02 - Test Salwa</h1>
-    </div>
+    <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
+      <div className = "bg-light container">     
+        <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home"><img alt="" src={logo} width="250" height="56" 
+                            className="d-inline-block align-top" /></Navbar.Brand>
+            </Container>
+        </Navbar>  
+      </div> 
+    </ThemeProvider>    
   );
 }
 export default App;
