9f99c045b3f24a9d6bb2592ccf0a1b532c46d666
saadlarbi <saadux@gmail.com>
2022-05-12 02:58:04 +0200

    agg34

--- src/component/carousel.jsx
+++ src/component/carousel.jsx
@@ -7,17 +7,17 @@ export default function carousel() {
         <Carousel variant="dark" fade>
             <Carousel.Item interval={3000}>
                 <img src={gps02} alt="Gps01"/>
-                <Carousel.Caption>
-                    <h3>First Slide</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in facilis corporis optio. Dolore, hic labore pariatur quo ad similique!</p>
-                </Carousel.Caption>
+                {/* <Carousel.Caption> */}
+                    {/* <h3>First Slide</h3> */}
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in facilis corporis optio. Dolore, hic labore pariatur quo ad similique!</p> */}
+                {/* </Carousel.Caption> */}
             </Carousel.Item>
             <Carousel.Item interval={3000}>
                 <img src={gps01} alt="Gps02"/>
-                <Carousel.Caption>
-                    <h3>Second Slide</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in facilis corporis optio. Dolore, hic labore pariatur quo ad similique!</p>
-                </Carousel.Caption>
+                {/* <Carousel.Caption> */}
+                    {/* <h3>Second Slide</h3> */}
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in facilis corporis optio. Dolore, hic labore pariatur quo ad similique!</p> */}
+                {/* </Carousel.Caption> */}
             </Carousel.Item>
         </Carousel>
     )
