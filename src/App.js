9e0f424e397ed1d0c1af013673692344dcbc6186
saadlarbi <saadux@gmail.com>
2022-05-20 21:27:37 +0200

    agg39

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider } from 'react-bootstrap';
+
 import './App.css';
 import Carousel from './component/carousel.jsx';
 import Navbar from './component/navbar.jsx';
@@ -8,26 +13,23 @@ import Footer from './component/Footer.jsx';
 import Home from './component/Home.js';
 import Menu from './component/menu.js';
 import NotFoundPage from './component/NotFoundPage.js';
-import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import {ThemeProvider} from 'react-bootstrap';
 
 function App() {
   return (
-      <div>
+    <div>
       <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
         <Router>
-        <Navbar />
+          <Navbar />
           <Routes>
             <Route path="/" element={<><Carousel /><Home /></>} ></Route>
             <Route path="/menu" element={<Menu />} ></Route>
             <Route path="/solutions" element={<Cards />} ></Route>
             <Route element={<NotFoundPage />} ></Route>
-        </Routes>
-          </Router>
-        <Footer /> 
-        </ThemeProvider>
-      </div>   
+          </Routes>
+        </Router>
+        <Footer />
+      </ThemeProvider>
+    </div>
   );
 }
 export default App;
--- src/component/cards.jsx
+++ src/component/cards.jsx
@@ -20,13 +20,13 @@ export default function cards() {
                             <Card.Title><b>Fmb920</b></Card.Title>
                             <Card.Subtitle className="mb-2 text-muted"><b className="text-primary">Traceur Léger</b></Card.Subtitle>
                         </Card.Header>
-                        <Card.Img variant="top" src={t920} style={{ width: '18rem' }} />
+                        <Card.Img variant="top" src={t920} />
                         <Card.Body>
                             <Card.Text>
-                                Le Fmb920est un petit traceur professionnel avec antennes GPS et Batterie intégrées,
+                                Le Fmb920 est un petit traceur professionnel avec antenne GPS et Batterie intégrées,
                                 il permet la collecte des positions et la coupure de moteur à distance,
                                 c’est la solution efficace contre les abus d’utilisation pour les particuliers,
-                                les gestionnaires de parcs
+                                les gestionnaires de parcs légers.
                             </Card.Text>
                         </Card.Body>
                         <Card.Footer className="text-muted text-center text-md-right"><h2><b>Prix : 900 Dhs</b></h2></Card.Footer>
@@ -38,13 +38,13 @@ export default function cards() {
                             <Card.Title><b>Fmb140</b></Card.Title>
                             <Card.Subtitle className="mb-2 text-muted"><b className="text-danger">Solution Avancée</b></Card.Subtitle>
                         </Card.Header>
-                        <Card.Img variant="top" src={t140} style={{ width: '18rem' }} />
+                        <Card.Img variant="top" src={t140}/>
                         <Card.Body>
                             <Card.Text>
-                                Le FMB140 et la solution complète pour les professionnels de la logistique,
-                                ou un seul boitier peut faire différentes taches, il est conçu pour les solutions
-                                complexes, il permet la géolocalisation, la lecture OBD, DTS, FUEL CAN DATA,
-                                Tachéographe. une solution tout en un centralisée et pratique
+                                Le FMB140 est la solution complète pour les professionnels de la logistique,
+                                Ce Boitier seul peut recourir à différentes Besoins, il est conçu pour les solutions
+                                complexes ou la géolocalisation va en parallèle a d'autres tâches en monitoring.
+                                Une solution tout en un centralisée et pratique.
                             </Card.Text>
                         </Card.Body>
                         <Card.Footer className="text-muted text-center text-md-right"><h2><b>Prix : 1 400 Dhs</b></h2></Card.Footer>
@@ -56,13 +56,12 @@ export default function cards() {
                             <Card.Title><b>Gh5200</b></Card.Title>
                             <Card.Subtitle className="mb-2 text-muted"><b className="text-success">Traceur Personnel</b></Card.Subtitle>
                         </Card.Header>
-                        <Card.Img variant="top" src={gh5200} style={{ width: '18rem' }} />
+                        <Card.Img variant="top" src={gh5200} />
                         <Card.Body>
                             <Card.Text>
-                                Ce Nouveau boitier relevé les positions et mouvement du personnel
-                                et l’envoie au serveur pour une gestion optimale des équipes sur le terrain;
-                                il a l’avantage d'avoir une batterie intégrée de grande autonomie permettant
-                                une gestion optimale des agents
+                                Ce Nouveau boitier releve les positions des Agents en Service pour les envoie au serveur, 
+                                Il a l’avantage d'avoir une batterie intégrée à grande autonomie permettant une gestion 
+                                optimale des équipes sur le terrain; 
                             </Card.Text>
                         </Card.Body>
                         <Card.Footer className="text-muted text-center text-md-right"><h2><b>Prix : 1 200 Dhs</b></h2></Card.Footer>
